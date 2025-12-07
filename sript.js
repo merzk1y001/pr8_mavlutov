acba221cc5c482e82a8d3221485871a9d7b2e29e
merzk1y001 <timur.mavlutov00@mail.ru>
2025-12-07 22:43:50 +0300

    added password generator

---
+++ sript.js
@@ -0,0 +1,29 @@
+function GeneratePassword() {
+  lcase = document.getElementById("lcase");
+  ucase = document.getElementById("ucase");
+  resultText = document.getElementById("resultText");
+  count = document.getElementById("count");
+  let lcasesym = "abcdefghijklmnopqrstuvwxyz";
+  let ucasesym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let available = "";
+  if (count.value == "") {
+    return;
+  }
+  let index = Number(count.value);
+  if (lcase.checked) {
+    available = available + lcasesym;
+  }
+  if (ucase.checked) {
+    available = available + ucasesym;
+  }
+  if (available.length == 0) return;
+  let result = "";
+  for (let i = 0; i < index; i++) {
+    result = result + available[random(0, available.length)];
+  }
+  resultText.value = result;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
